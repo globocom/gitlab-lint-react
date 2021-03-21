e307aee9f9a012ffaf721836f732eec44deaf90c
Marcelo Jorge Vieira <metal@alucinados.com>
2021-03-21 17:19:28 -0300

    prettier

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Marcelo Jorge Vieira
 // Licensed under the BSD 3-Clause License
 
-import React, { useState, useEffect, useCallback  } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Pagination from "@material-ui/lab/Pagination";
 import {
@@ -11,9 +11,9 @@ import {
   ListItemText,
   Tooltip,
   Typography,
-  Input
+  Input,
 } from "@material-ui/core";
-import debounce from 'lodash.debounce';
+import debounce from "lodash.debounce";
 
 import GitlabLintHttpClient from "../GitlabLintHttpClient";
 import Loading from "../Loading";
@@ -69,15 +69,14 @@ const Projects = () => {
     setPage(value);
     fetchData({ query: { page: value, q: searchInput } });
   };
-	const debouncedSearch = useCallback(
-		debounce(value => fetchData({ query: { page, q: value } }), 500),
-		[],
-	);
-  const handleChangeSearch = (value)  => {    
-    setSearchInput(value);  
+  const debouncedSearch = useCallback(
+    debounce((value) => fetchData({ query: { page, q: value } }), 500),
+    []
+  );
+  const handleChangeSearch = (value) => {
+    setSearchInput(value);
     debouncedSearch(value);
-    
-  }
+  };
   const [rows, setData] = useState({});
   const fetchData = ({ query }) => {
     GitlabLintHttpClient("GET_ALL", { entity: "projects", query: query })
@@ -101,9 +100,13 @@ const Projects = () => {
         Projects
       </Typography>
       <List>
-      <form className={classes.search} noValidate autoComplete="off">
-        <Input placeholder="Search for a project.." value={searchInput}  onChange={e => handleChangeSearch(e.target.value)}/>
-      </form>
+        <form className={classes.search} noValidate autoComplete="off">
+          <Input
+            placeholder="Search for a project.."
+            value={searchInput}
+            onChange={(e) => handleChangeSearch(e.target.value)}
+          />
+        </form>
         {rows.map((row) => {
           return (
             <ListItem
